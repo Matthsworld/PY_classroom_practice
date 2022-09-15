4552982e27b4cf4d0421b921a3eb0e3d14de84b4
matthsworld <matthsworld@gmail.com>
2022-09-15 16:53:41 -0700

    update: update which_prize.py file

--- which_prize.py
+++ which_prize.py
@@ -1,3 +1,4 @@
+'''
 Practice: Which Prize
 Write an if statement that lets a competitor know which of these prizes they won based on the number of points they scored, which is stored in the integer variable points.
 
@@ -11,8 +12,9 @@ All of the lower and upper bounds here are inclusive, and points can only take o
 In your if statement, assign the result variable to a string holding the appropriate message based on the value of points. If they've won a prize, the message should state "Congratulations! You won a [prize name]!" with the prize name. If there's no prize, the message should state "Oh dear, no prize this time."
 
 Note: Feel free to test run your code with other inputs, but when you submit your answer, only use the original input of points = 174. You can hide your other inputs by commenting them out.
-
-points = 174  # use this input to make your submission
+'''
+'''points = 174  # use this input to make your submission'''
+points = int(input())
 
 # # write your if statement here
 # 1 - 50	wooden rabbit
