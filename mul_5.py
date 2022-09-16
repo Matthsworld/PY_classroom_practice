6323d0d742f5e780ef48ca2dcf6686a6e41a118d
matthsworld <matthsworld@gmail.com>
2022-09-16 03:26:05 -0700

    ft: add more file

---
+++ mul_5.py
@@ -0,0 +1,6 @@
+'''
+Write a for loop below that will print out every whole number that is a multiple of 5 and less than or equal to 30.
+'''
+# Write a for loop using range() to print out multiples of 5 up to 30 inclusive
+for number in range(5, 35, 5):
+    print(number)
